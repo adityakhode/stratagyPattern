42c58aa950cc01b26876cc85a1856f29646b4748
Aditya Khode <113977001+adityakhode@users.noreply.github.com>
2025-12-12 18:47:49 +0000

    interface added

--- OBSERVERPATTERN/src/main/java/observer/EventManager.java
+++ OBSERVERPATTERN/src/main/java/observer/EventManager.java
@@ -3,7 +3,7 @@ package observer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EventManager {
+public class EventManager implements Subject {
     private final List<Observer> observers = new ArrayList<>();
 
     public void attach(Observer o) { observers.add(o); }
